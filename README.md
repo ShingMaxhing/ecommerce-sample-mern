301ab330ce89b755b583c462a115cc7eb25185f6
Sheila Mae Apusen <sma101philippines@gmail.com>
2022-06-06 20:03:19 +0800

    New Pages

--- README.md
+++ README.md
@@ -7,12 +7,58 @@
 3. Create React App
 4. Create Git Reporsitory
 5. List Products
-   
+
    i. create products array
    ii. add product images
    iii. render products
    iv. style products
+
 6. Add routing
    i. npm i react-router-dom
    ii. create route for home screen
    iii. create router for produt screen
+
+7. Create Node.js Server
+
+   1. run npm init in root folder
+   2. Update package.json set type:module
+   3. Add .js to imports
+   4. npm install express
+   5. create server.js
+   6. add start command as node backend/server.js
+   7. require express
+   8. create route for / return backend is ready.
+   9. move product.js from frontend to backend
+   10. create route for /api/products
+   11. return products
+   12. run npm start
+
+8. Fetch Products From Backend
+
+   1. set proxy in package.json
+   2. npm install axios
+   3. use state hook
+   4. use effect hook
+   5. use reducer hook
+
+9. Manage State By Reducer Hook
+
+   1. define reducer
+   2. update fetch data
+   3. get state from usReducer
+
+10. Add bootstrap UI Framework
+
+11. npm install react-bootstrap bootstrap
+12. update App.js
+
+13. Create Product and Rating Component
+
+14. create Rating component
+15. Create Product component
+16. Use Rating component in Product component
+
+17. Create Product Details Screen
+
+18. fetch product from backend
+19. create 3 columns for image, info and action
--- react2/src/App.js
+++ react2/src/App.js
@@ -1,32 +1,38 @@
 import data from './data';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './Pages/HomeScreen';
+import ProductScreen from './Pages/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { NavbarBrand } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
-    <div>
-      <header className="App-header">
-        <a href='/'>amazon</a>
-      </header>
-    <main>
-      <h1>Featured Products</h1>
-      <div className='products'>
-      {
-        data.products.map(product => (
-        <div className='product' key={product.slug}>
-          <a href={`/product/${product.slug}`}>
-          <img src={product.image} alt={product.name}/>
-          </a>
-          <div className='product-info'>
-          <a href={`/product/${product.slug}`}>
-            <p>{product.name}</p>
-          </a>
-            <p><strong>${product.price}</strong></p>
-            <button>Add to cart</button>
-          </div>
-        </div>))
-      }
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <header className="App-header">
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <NavbarBrand>amazon</NavbarBrand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container className="mt-3">
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
-    </main>
-    </div>
+    </BrowserRouter>
   );
 }
 
